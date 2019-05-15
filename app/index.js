215d45b911f95e17277c49971047d543f82fe81b
Godsgift Akari <giftakari@yahoo.com>
2019-05-15 11:40:04 +1000

    Add Battle component

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pupolar from './components/Pupolar'
+import Battle from './components/Battle'
 import './index.css'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Pupolar/>
+        {/* //<Pupolar/> */}
+        <Battle/>
+
       </div>
     )
   }
