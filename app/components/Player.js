51fdcc282bad4fad3d2e8e8b16a373ba019ece39
Godsgift Akari <giftakari@yahoo.com>
2019-05-15 11:59:45 +1000

    Styled Battle component

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,32 +1,6 @@
-import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
-import PropTypes from 'prop-types'
 
-function Instructions () {
-  return (
-    <div className='instructions-container'>
-      <h1 className='center-text header-lg'>
-        Instructions
-      </h1>
-      <ol className='container-sm grid center-text battle-instructions'>
-        <li>
-          <h3 className='header-sm'>Enter two Github users</h3>
-          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>Battle</h3>
-          <FaFighterJet className='bg-light' color='#727272' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>See the winners</h3>
-          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
-        </li>
-      </ol>
-    </div>
-  )
-}
 
-class PlayerInput extends React.Component {
+export default class PlayerInput extends React.Component {
   constructor(props) {
     super(props)
 
@@ -79,16 +53,4 @@ class PlayerInput extends React.Component {
 PlayerInput.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired
-}
-
-export default class Battle extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Instructions />
-        <PlayerInput label='Label' onSubmit={(value) =>
-        console.log('value', value)} />
-      </React.Fragment>
-    )
-  }
 }
